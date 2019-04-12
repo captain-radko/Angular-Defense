b3e39b532b44eec89b7026cf6644e76616b24688
capitan-radko <water235641@abv.bg>
2019-04-12 16:50:16 +0300

    styling update

--- CookBook/cook-book/src/app/components/recipes/add-recipe/add-recipe.component.ts
+++ CookBook/cook-book/src/app/components/recipes/add-recipe/add-recipe.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { CdkTextareaAutosize } from '@angular/cdk/text-field';
+import { Component, NgZone, ViewChild, OnInit } from '@angular/core';
+import { take } from 'rxjs/operators';
 
 @Component({
   selector: 'app-add-recipe',
@@ -6,13 +8,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-recipe.component.css']
 })
 export class AddRecipeComponent implements OnInit {
+  constructor(private ngZone: NgZone) { }
 
-  constructor() { }
+  @ViewChild('autosize') autosize: CdkTextareaAutosize;
+
+  triggerResize() {
+    // Wait for changes to be applied, then trigger textarea resize.
+    this.ngZone.onStable.pipe(take(1))
+      .subscribe(() => this.autosize.resizeToFitContent(true));
+  }
 
   ngOnInit() {
   }
 
-  create(){
-    
+  create() {
+
   }
 }
