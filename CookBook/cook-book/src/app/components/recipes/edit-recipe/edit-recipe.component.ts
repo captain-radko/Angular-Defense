93d89a8608930aec0b46ddf7e101b7d3162338fa
capitan-radko <water235641@abv.bg>
2019-04-18 14:44:56 +0300

    edit form validation added

--- CookBook/cook-book/src/app/components/recipes/edit-recipe/edit-recipe.component.ts
+++ CookBook/cook-book/src/app/components/recipes/edit-recipe/edit-recipe.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { RecipeService } from 'src/app/services/recipe-crud.service';
 import { ActivatedRoute, Router } from '@angular/router';
-import { NgForm } from '@angular/forms';
+import { NgForm, FormGroup, FormControl, Validators } from '@angular/forms';
 import { IRecipe } from 'src/app/models/recipe';
 
 @Component({
@@ -12,10 +12,20 @@ import { IRecipe } from 'src/app/models/recipe';
 export class EditRecipeComponent implements OnInit {
   public recipe: IRecipe;
   constructor(private recipeService: RecipeService, private route: ActivatedRoute, private router: Router) { }
-
+  editRecipeValidator: FormGroup;
   @ViewChild('editForm') editForm: NgForm;
 
   ngOnInit() {
+    this.editRecipeValidator = new FormGroup({
+      image: new FormControl('', [Validators.required]),
+      title: new FormControl('', [Validators.required, Validators.minLength(4), Validators.maxLength(16)]),
+      ingredients: new FormControl('', [Validators.required, Validators.minLength(4)]),
+      recipeText: new FormControl('', [Validators.required])
+    });
+  }
+
+  public hasError = (controlName: string, errorName: string) => {
+    return this.editRecipeValidator.controls[controlName].hasError(errorName);
   }
 
   edit() {
